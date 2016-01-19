6df6b8fdd60ee2c1fd387d7368bc04d7b7d95b0e
dimitris <dimitris.rongotis@gmail.com>
2016-01-19 11:58:10 +0200

    all endpoints implemented as simple functions

--- src/snapshot.py
+++ src/snapshot.py
@@ -45,15 +45,68 @@ class SnapshotRequest(object):
         response = self.session_get(url)
         return [h['hash'] for h in response.json()['result']]
 
-
-    def list_all_binary_packages_for_this_package_at_version(self,version):
+    def list_all_binary_packages_for_this_package_at_version(self, version):
         """
         URL: /mr/package/<package>/<version>/binpackages
         http status codes: 200 500 404 304
         summary: list all binary packages associated with this source package at that version
         :param version:
-        :return:
+        :return: **temporary** return dict
         """
         url = url_join(BASE_URL, 'mr/package/{package}/{version}/binpackages'.format(package=self.package_name, version=version))
         response = self.session_get(url)
         return response.json()['result']
+
+    def list_all_files_associated_with_a_binary_package(self, version, binpkg, binversion):
+        """
+        URL: /mr/package/<package>/<version>/binfiles/<binpkg>/<binversion>
+        Options: fileinfo=1  includes fileinfo section
+        http status codes: 200 500 404 304
+        summary: list all files associated with a binary package
+        :param version: the version of binpkg
+        :param binpkg: binpackage you get from list_all_binary_packages_for_this_package_at_version(self, version)
+        :param binversion: version of **binpkg**
+        :return:
+        """
+        url = url_join(BASE_URL,
+                       '/mr/package/{package}/{version}/binfiles/{binpkg}/{binversion}'.format(package=self.package_name, version=version, binpkg=binpkg, binversion=binversion))
+        response = self.session_get(url)
+        return response.json()
+
+    def list_all_files_associated_with_this_source_package_at_that_version(self, version):
+        """
+        URL: /mr/package/<package>/<version>/allfiles
+        Options: fileinfo=1  includes fileinfo section
+        http status codes: 200 500 404 304
+        summary: list all files associated with this source package at that version
+        :param version: the version of the package
+        :return:
+        """
+        url = url_join(BASE_URL, '/mr/package/{package}/{version}/allfiles'.format(package=self.package_name, version=version))
+        response = self.session_get(url)
+        return response.json()
+
+    def find_binary_package_versions_and_corresponding_source_names_and_versions(self, binary):
+        """
+        URL: /mr/binary/<binary>/
+        http status codes: 200 500 404 304
+        summary: find binary package versions and corresponding source names and versions
+        :param binary: binary package name
+        :return:
+        """
+        url = url_join(BASE_URL, '/mr/binary/{binary}/'.format(binary=binary))
+        response = self.session_get(url)
+        return response.json()
+
+    def info_from_hash(self,the_hash):
+        """
+        URL: /mr/file/<hash>/info
+        http status codes: 200 500 404 304
+        :param hash:
+        :return:
+        """
+        url = url_join(BASE_URL,'/mr/file/{the_hash}/info'.format(the_hash=the_hash))
+        response=self.session_get(url)
+        return response.json()
+
+
