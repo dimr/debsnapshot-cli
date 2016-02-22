b232845d57ea8461e4ef3ed75b08f008283a9e38
dimitris <dimitris.rongotis@gmail.com>
2016-02-22 11:00:30 +0200

    added install_requires in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
-#from distutils.core import setup
-from setuptools import setup,find_packages
+# from distutils.core import setup
+from setuptools import setup, find_packages
+
+with open('requirements.txt', 'r') as f:
+    required = f.read().splitlines()
 
 setup(
     name='debsnapshot-cli',
@@ -10,11 +13,12 @@ setup(
     author='Dimitris Rongotis',
     author_email='dimitris.rongotis@gmail.com',
     description='',
+    install_requires=required,
     entry_points={
         'console_scripts': [
             'debsnapshot-cli=src.cli:main'
         ]
     }
-   # scripts=['bin/debsnapshot-cli']
+    # scripts=['bin/debsnapshot-cli']
 
 )
