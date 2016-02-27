ddfa4ced1dbc2539e0305e4edb942d7500965d77
dimitris <dimitris.rongotis@gmail.com>
2016-02-27 16:33:37 +0200

    refactored cli.py

--- src/cli.py
+++ src/cli.py
@@ -15,7 +15,7 @@ SNAPSHOT_FILE = 'snapshot.list'
 # logging.getLogger("requests").setLevel(logging.CRITICAL)
 loggerCLI = logging.getLogger(__file__)
 # logging.basicConfig(level=logging.DEBUG)
-loggerCLI.setLevel(logging.DEBUG)
+loggerCLI.setLevel(logging.CRITICAL)
 
 
 def check(time_stamp, package=None, version=None):
@@ -36,11 +36,11 @@ def check(time_stamp, package=None, version=None):
         print("you should not run this script as ROOT")
 
 
-def main(args=None):
+def create_parser(args=None):
     parser = argparse.ArgumentParser(description="find package information in debian.snapshot.org")
 
     # parser.add_argument('architecture', nargs='?', type=str, default=None, help='set the target version of the package you wish you')
-    parser.add_argument("package_version", nargs='?', default=None, help='set the target version of the package you wish you')
+    parser.add_argument("package_version", nargs=argparse.OPTIONAL, default=None, help='set the target version of the package you wish you')
     parser.add_argument("package_name", help="write the package name you wish to find", type=str, default=None)
     # optional
     parser.add_argument('-lb', '--all-binary-versions', help='find binary package versions and corresponding source names and versions', action='store_true')
@@ -52,9 +52,13 @@ def main(args=None):
     parser.add_argument('-arch', '--architecture', nargs=1, type=str, help='define system architecture')
     # parser.add_argument('-all-packages', help='all packages', action='store_true')
     parser.add_argument('-v', '--version', help='print debsnapshot-cli version', action='version', version=' : '.join((__title__, __version__)))
-    parser.add_argument('-w','--write-to-file',help="add url entry to /etc/apt/sources.list.d/snapshot.list",action="store_true")
-    args = parser.parse_args(args)
+    parser.add_argument('-w', '--write-to-file', help="add url entry to /etc/apt/sources.list.d/snapshot.list", action="store_true")
+    # args = parser.parse_args(args)
+    return parser.parse_args(args)
 
+
+def main(args):
+    args = create_parser(args)
     if args.package_version is None:
         if args.all_source_versions:
             loggerCLI.debug(args)
@@ -90,7 +94,7 @@ def main(args=None):
         elif args.first_seen and args.architecture:
             loggerCLI.debug(args)
             package = SnapshotRequest(args.package_name)
-            result = package.info_from_hash(args.package_version, args.architecture[0])
+            result = package.info_from_hash(args.package_version, check_port(args.architecture[0]))
             result['result'][0].setdefault('hash', result['hash'])
             print('\n')
             print(tabulate(result['result'], headers='keys'))
@@ -113,50 +117,6 @@ def main(args=None):
                 elif ans == 'n':
                     print('\nBye!')
                     sys.exit()
-                    # NOT IMPLEMENTED FOR NOW
-    # elif args.all_files:
-    #     package = SnapshotRequest(args.package_name)
-    #     print(args)
-    #     print('---------------------------')
-    #     # num_parameters = len(getattr(args, 'all_files'))
-    #     # version = getattr(args, 'all_files')[0]
-    #     if args.architecture is None:
-    #         pprint.pprint(package.list_all_files_associated_with_this_source_package_at_that_version(args.version[0]))
-    #         exit()
-    #     # arch = getattr(args, 'all_files')[1]
-    #     architecture = check_port(args.architecture[0])
-    #     # pprint.pprint(package.list_all_files_associated_with_this_source_package_at_that_version(args.version[0], architecture), width=1)
-    #     result = package.list_all_files_associated_with_this_source_package_at_that_version(args.version[0], architecture)
-    #     print(tabulate(result['binaries'][0]['files'], tablefmt='grid', headers='keys'))
-    #     # print getattr(args, 'all_files')[1]
-    # elif args.info:
-    #     package = SnapshotRequest(args.package_name)
-    #     result = package.info_from_hash(args.version[0], args.architecture[0])
-    #     result['result'][0].setdefault('hash', result['hash'])
-    #     print('\n')
-    #     print(tabulate(result['result'], headers='keys'))
-    #     print('\n')
-    #     time_stamp = result['result'][0]['first_seen']
-    #     url = SNAPSHOT_URL.format(time_stamp=time_stamp)
-    #     result = {'URL': url}
-    #     print(tabulate([result], headers="keys"))
-    #     # print('URL: ' + SNAPSHOT_URL.format(time_stamp=time_stamp))
-    #     print('\n')
-    #     print("Append this URL to /etc/apt/sources.list.d/snapshot.list?")
-    #     proceed = raw_input("do you want to continue [y/n/Y/N]:")
-    #     ans = lambda x: x.strip().lower()
-    #     if ans(proceed) == 'y':
-    #         print("Please enter your ROOT password:")
-    #         check(time_stamp, package=args.package_name, version=args.version[0])
-    #     elif ans == 'n':
-    #         print('\nBye!')
-    #         sys.exit()
-    # elif args.package_version:
-    #     loggerCLI.debug(args)
-    # elif not args.package_version and args.package_name:
-    #     loggerCLI.debug(args)
-    #
-    return 0
 
 
 if __name__ == '__main__':
